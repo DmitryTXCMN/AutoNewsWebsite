11ef0dc8a19bd846e42d1709192e719eb4d8c862
Mansur <ahmatowmansur@gmail.com>
2021-12-24 13:07:03 +0300

    q

--- AutoNews/AutoNews/Controllers/ProfileController.cs
+++ AutoNews/AutoNews/Controllers/ProfileController.cs
@@ -7,6 +7,7 @@ public class ProfileController : Controller
 {
     [Authorize]
     public IActionResult Index() => View(HttpContext.Items["User"]);
+
     [Authorize]
     public IActionResult Edit() => View(HttpContext.Items["User"]);
 }
