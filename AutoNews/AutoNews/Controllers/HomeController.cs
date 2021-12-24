95d39e4639af434db9ca7c72c0cdab5efdca82f6
Mansur <ahmatowmansur@gmail.com>
2021-12-24 17:19:40 +0300

    q

--- AutoNews/AutoNews/Controllers/HomeController.cs
+++ AutoNews/AutoNews/Controllers/HomeController.cs
@@ -9,11 +9,31 @@ public class HomeController : Controller
 {
     private readonly AutoNewsContext _dataContext;
 
-    public HomeController(AutoNewsContext dataContext) => 
+    public HomeController(AutoNewsContext dataContext) =>
         _dataContext = dataContext;
 
     public IActionResult Index() =>
-        View(_dataContext.News.OrderBy(n => n.Likes));
+        View(_dataContext.News.OrderBy(n => n.Likes).Select(n => new BetterNews
+        {
+            Id = n.Id,
+            Title = n.Title,
+            Text = n.Text,
+            LogoUrl = n.LogoUrl,
+            Likes = n.Likes,
+            Date = n.Date,
+            Creator = _dataContext.Users.FirstOrDefault(u => u.Id == n.CreatorId)
+        }));
+
+    public readonly struct BetterNews
+    {
+        public int Id { get; init; }
+        public string Title { get; init; }
+        public string Text { get; init; }
+        public string LogoUrl { get; init; }
+        public int Likes { get; init; }
+        public DateTime Date { get; init; }
+        public User? Creator { get; init; }
+    }
 
     public IActionResult Privacy() =>
         View();
